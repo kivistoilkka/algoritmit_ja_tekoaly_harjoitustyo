301cef6c87be99f84690fa3b4f73d04b402757b1
Ilkka Kivistö <ilkka.kivisto@gmail.com>
2023-09-12 21:20:31 +0300

    First steps in creating Huffman tree creator

--- src/huffman_coder.py
+++ src/huffman_coder.py
@@ -1,19 +1,45 @@
 from collections import Counter
+from heapq import heappush, heappop
+
 
 class HuffmanCoder:
     def __init__(self) -> None:
         pass
 
     def encode(self, input_string: str) -> str:
-        return input_string
+        return (input_string, input_string)
 
     def calculate_frequencies(self, input_string: str) -> dict:
         output = Counter(input_string)
-        for symbol in output:
-            output[symbol] = output[symbol]/len(input_string)
         return output
 
+    def create_huffman_tree(self, symbols_and_weights: dict[str, int]) -> list:
+        return list(symbols_and_weights.values())
+
+
 if __name__ == "__main__":
-    coder = HuffmanCoder()
-    result = coder.calculate_frequencies('How to code this string with Huffman coding?')
-    print(result)
+    # coder = HuffmanCoder()
+    # test_string = 'How to code this string with Huffman coding?'
+    # print(test_string.encode())
+    # result = coder.calculate_frequencies(test_string)
+    # print(result)
+
+    # keko = []
+    # heappush(keko,5)
+    # heappush(keko,3)
+    # heappush(keko,8)
+    # heappush(keko,7)
+    # print(keko[0]) # 3
+    # heappop(keko)
+    # print(keko[0]) # 5
+    # print(type(keko))
+
+    # symbols_and_weights = {
+    #     'H': 2,
+    #     'o': 4,
+    #     'w': 2,
+    #     ' ': 7,
+    #     't': 4
+    # }
+    # print(list(symbols_and_weights.values()))
+    pass
--- src/tests/huffman_coder_test.py
+++ src/tests/huffman_coder_test.py
@@ -1,40 +1,57 @@
 import unittest
 from huffman_coder import HuffmanCoder
 
+
 class TestHuffmanCoder(unittest.TestCase):
     def setUp(self):
         self.coder = HuffmanCoder()
 
-    def test_encode_returns_string(self):
+    def test_encode_returns_tuple_of_string_and_string(self):
         result = self.coder.encode('Hello world')
-        self.assertTrue(isinstance(result, str))
+        self.assertTrue(isinstance(result, tuple))
+        self.assertTrue(len(result) == 2)
+        self.assertTrue(isinstance(result[0], str))
+        self.assertTrue(isinstance(result[1], str))
 
     def test_calculate_frequencies_returns_dictionary(self):
-        result = self.coder.calculate_frequencies('How to code this string with Huffman coding?')
+        result = self.coder.calculate_frequencies(
+            'How to code this string with Huffman coding?')
         self.assertTrue(isinstance(result, dict))
 
     def test_calculate_frequencies_returns_symbols_with_frequencies(self):
-        result = self.coder.calculate_frequencies('How to code this string with Huffman coding?')
+        result = self.coder.calculate_frequencies(
+            'How to code this string with Huffman coding?')
         expected = {
-            'H': 2/44,
-            'o': 4/44,
-            'w': 2/44,
-            ' ': 7/44,
-            't': 4/44,
-            'c': 2/44,
-            'd': 2/44,
-            'e': 1/44,
-            'h': 2/44,
-            'i': 4/44,
-            's': 2/44,
-            'r': 1/44,
-            'n': 3/44,
-            'g': 2/44,
-            'u': 1/44,
-            'f': 2/44,
-            'm': 1/44,
-            'a': 1/44,
-            '?': 1/44
+            'H': 2,
+            'o': 4,
+            'w': 2,
+            ' ': 7,
+            't': 4,
+            'c': 2,
+            'd': 2,
+            'e': 1,
+            'h': 2,
+            'i': 4,
+            's': 2,
+            'r': 1,
+            'n': 3,
+            'g': 2,
+            'u': 1,
+            'f': 2,
+            'm': 1,
+            'a': 1,
+            '?': 1
         }
 
         self.assertEqual(result, expected)
+
+    def test_create_huffman_tree_returns_list(self):
+        symbols_and_weights = {
+            'H': 2,
+            'o': 4,
+            'w': 2,
+            ' ': 7,
+            't': 4
+        }
+        result = self.coder.create_huffman_tree(symbols_and_weights)
+        self.assertTrue(isinstance(result, list))
