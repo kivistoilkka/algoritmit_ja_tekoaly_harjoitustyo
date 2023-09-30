c33da268281841a3763261bb8bbd1b685feae69f
Ilkka Kivistö <ilkka.kivisto@gmail.com>
2023-10-01 00:08:50 +0300

    Docstring for FileIO

--- src/utils/file_io.py
+++ src/utils/file_io.py
@@ -1,8 +1,20 @@
 class FileIO:
+    """Class handling file reading and writing
+    """
+
     def __init__(self) -> None:
         pass
 
-    def read_file(self, filename: str):
+    def read_file(self, filename: str) -> str:
+        """Method for reading text files, assumes that files are saved using ISO8859-1 encoding.
+
+        Args:
+            filename (str): Path and name of the file
+
+        Returns:
+            str: Text data read from the file
+        """
+
         try:
             with open(filename, encoding="ISO8859-1") as file:
                 data = file.read()
@@ -10,7 +22,16 @@ class FileIO:
         except OSError:
             return None
 
-    def write_file(self, data, filename):
+    def write_file(self, data: str, filename: str) -> bool:
+        """Method for writing data to text file using ISO8859-1 encoding.
+
+        Args:
+            data (str): Text data to be saved
+            filename (str): Path and name of the file to be written
+
+        Returns:
+            bool: True if writing is successful
+        """
         try:
             with open(filename, "w", encoding="ISO8859-1") as file:
                 file.write(data)
