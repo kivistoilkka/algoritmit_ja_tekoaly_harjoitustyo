72c8b573dbdccbceca1735f8748952c4de39633f
Ilkka Kivistö <ilkka.kivisto@gmail.com>
2023-10-07 18:18:59 +0300

    Huffman encoding and decoding using bits working

--- src/utils/huffman_coder.py
+++ src/utils/huffman_coder.py
@@ -1,7 +1,7 @@
 from collections import Counter
 from heapq import heappush, heappop, heapify
 
-from bitstring import BitArray, pack
+from bitstring import Bits, BitArray, ConstBitStream
 
 from ..entities.huffmantree_node import HuffmanTreeNode
 
@@ -13,14 +13,14 @@ class HuffmanCoder:
     def __init__(self) -> None:
         pass
 
-    def encode(self, input_string: str) -> tuple[bytes, bytes]:
+    def encode(self, input_string: str) -> tuple[BitArray, BitArray]:
         """Method for encoding input bytes with Huffman coding
 
         Args:
             input_string (str): Text to be encoded
 
         Returns:
-            tuple[bytes, bytes]: Tuple where first value is encoded data and second is encoded Huffman tree
+            tuple[BitArray, BitArray]: Tuple where first value is encoded Huffman tree and second is encoded data
         """
 
         input = input_string.encode(encoding='ISO8859-1')
@@ -30,22 +30,23 @@ class HuffmanCoder:
         print(table)
         encoded_data = self.huffman_encode_data(input, table)
         encoded_tree = self.encode_huffman_tree(tree)
-        return (encoded_data, encoded_tree)
+        return (encoded_tree, encoded_data)
 
-    def decode(self, encoded_data: bytes, encoded_tree: bytes) -> bytes:
+    def decode(self, encoded_tree: bytes, encoded_data: bytes) -> bytes:
         """Method for decoding Huffman coded data using given encoded Huffman tree
 
         Args:
-            encoded_data (bytes): Data to be encoded
             encoded_tree (bytes): Encoded Huffman tree that was used for encoding of data
+            encoded_data (bytes): Data to be encoded
 
         Returns:
             str: Decoded text in bytes
         """
 
-        tree = self.decode_huffman_tree(encoded_tree)
-        print()
-        return self.huffman_decode_data(encoded_data, tree)
+        tree_bits = ConstBitStream(encoded_tree)
+        data_bits = ConstBitStream(encoded_data)
+        tree = self.decode_huffman_tree(tree_bits)
+        return self.huffman_decode_data(data_bits, tree)
 
     def calculate_frequencies(self, input: bytes) -> dict:
         output = Counter(input)
@@ -80,7 +81,7 @@ class HuffmanCoder:
         if node is None:
             return
         if node.is_leaf():
-            table[node.symbol] = code
+            table[node.symbol] = Bits(bin=code)
             return
         self.add_node_to_table(node.left_child, table, code + '0')
         self.add_node_to_table(node.right_child, table, code + '1')
@@ -88,7 +89,7 @@ class HuffmanCoder:
     def huffman_encode_data(self, data: bytes, table: dict) -> BitArray:
         encoded_data = BitArray()
         for symbol in data:
-            encoded_symbol = bin(int(table[symbol], 2))
+            encoded_symbol = table[symbol]
             encoded_data.append(encoded_symbol)
         return encoded_data
 
@@ -97,7 +98,7 @@ class HuffmanCoder:
         encoded_tree = self.add_node_to_encoded_tree(root_node, tree)
         return encoded_tree
 
-    def add_node_to_encoded_tree(self, node: HuffmanTreeNode, encoded_tree: BitArray) -> str:
+    def add_node_to_encoded_tree(self, node: HuffmanTreeNode, encoded_tree: BitArray) -> BitArray:
         if node is None:
             return
         if node.is_leaf():
@@ -110,45 +111,55 @@ class HuffmanCoder:
             node.right_child, encoded_tree)
         return encoded_tree
 
-    def decode_huffman_tree(self, encoded_tree: bytes) -> HuffmanTreeNode:
+    def decode_huffman_tree(self, encoded_tree: ConstBitStream) -> HuffmanTreeNode:
         print('Tree to decode:')
-        print(encoded_tree)
+        print(encoded_tree.bin)
         print()
-        huffman_tree_root_and_remaining_code = self.add_node_to_decoded_tree(
-            encoded_tree[2:])
-        return huffman_tree_root_and_remaining_code[0]
+        huffman_tree_root = self.add_node_to_decoded_tree(encoded_tree)
+        return huffman_tree_root
 
-    def add_node_to_decoded_tree(self, code: bytes):
-        print(code)
-        byte = code[0]
-        #code = code[1:]
-        if byte == '1':
-            print('adding')
-            char = code[0:8]
-            code = code[1:]
-            return (HuffmanTreeNode(1, int(char, 2).to_bytes(1, byteorder='big', signed=False)), code) #TODO: Word size is now magic number (1)
-        print('going left')
-        left_child, code = self.add_node_to_decoded_tree(code)
-        print('going right')
-        right_child, code = self.add_node_to_decoded_tree(code)
-        return (HuffmanTreeNode(0, '#', left_child, right_child), code)
+    def add_node_to_decoded_tree(self, code: ConstBitStream):
+        bit = code.read(1)
+        if bit == bin(1):
+            #print('adding')
+            char = code.read(8)
+            #print('char:', int.from_bytes(char.tobytes()))
+            return HuffmanTreeNode(1, char.bytes)
+        #print('going left')
+        left_child = self.add_node_to_decoded_tree(code)
+        #print('going right')
+        right_child = self.add_node_to_decoded_tree(code)
+        return HuffmanTreeNode(0, '#', left_child, right_child)
 
-    def huffman_decode_data(self, encoded_data: bytes, tree: HuffmanTreeNode) -> bytes:
+    def huffman_decode_data(self, encoded_data: ConstBitStream, tree: HuffmanTreeNode) -> bytes:
         decoded_data = b''
-        i = 0
+        #i = 0
         node = tree
-        for i in range(0, len(encoded_data)):
-            print('next byte:', end='')
-            print(encoded_data[i])
-            if encoded_data[i] == 0 and node.left_child:
+        # for i in range(0, len(encoded_data)):
+        #     print('next byte:', end='')
+        #     print(encoded_data.peek(1))
+        #     if encoded_data[i] == 0 and node.left_child:
+        #         node = node.left_child
+        #     elif node.right_child:
+        #         node = node.right_child
+        #     if node.is_leaf():
+        #         print('symbol:', end='')
+        #         print(node.symbol)
+        #         decoded_data = decoded_data + bytes(node.symbol)
+        #         node = tree
+        while True:
+            current_bit = encoded_data.read(1)
+            print('current bit:', current_bit)
+            if current_bit == bin(0) and node.left_child:
                 node = node.left_child
             elif node.right_child:
                 node = node.right_child
             if node.is_leaf():
-                print('symbol:', end='')
-                print(node.symbol)
-                decoded_data = decoded_data + bytes(node.symbol)
+                print('symbol:', node.symbol)
+                decoded_data += node.symbol
                 node = tree
+            if encoded_data.pos == len(encoded_data):
+                break
         return decoded_data
 
 
@@ -158,21 +169,23 @@ if __name__ == "__main__":
     test_string = 'How to code this string with Huffman coding?'
     print(BitArray(test_string.encode(encoding='ISO8859-1')).bin)
     encoded = coder.encode(test_string)
-    print()
-    print('Encoded data:')
-    print(encoded[0].bin)
 
     print()
     print('Encoded tree:')
+    print(encoded[0].bin)
+
+    print()
+    print('Encoded data:')
     print(encoded[1].bin)
 
     print()
     print('Combined:')
-    print((encoded[0] + encoded[1]).bin)
+    print((encoded[0]+ encoded[1]).bin)
 
+    print()
     decoded_data = coder.decode(encoded[0], encoded[1])
     print()
-    print(decoded_data)    
+    print(decoded_data)
 
     # test_string = 'How to code this string with Huffman coding?'
 
